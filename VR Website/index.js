a6a6b4819a3ecb41601d50169c13e5d6d080efdb
junaid1233 <muhamadjunaid27@gmail.com>
2024-08-01 03:13:04 +0500

    push

---
+++ VR Website/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const yearSpan = document.getElementById('year');
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+});
